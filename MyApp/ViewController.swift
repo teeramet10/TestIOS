7fc82445671fb9bf6af9bdfda6bf3d589faa4bba
teeramet kanchanapiroj <teeramet@192.168.86.128>
2017-11-09 08:36:15 -0800

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by teeramet kanchanapiroj on 11/9/2560 BE.
+//  Copyright © 2560 teeramet kanchanapiroj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
